01eb20006de4a0ea21a36fd73db21389d0e8906e
skllig <2228518550@qq.com>
2020-12-03 07:49:53 +0800

    completed proj1gold

---
+++ proj1gold/TestArrayDequeGold.java
@@ -0,0 +1,41 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestArrayDequeGold {
+
+    @Test
+    public void testSAD() {
+        StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> solution = new ArrayDequeSolution<>();
+
+        String message = "";
+        for (int i = 0; i < 31; i++) {
+            double flag = StdRandom.uniform();
+            if (flag > 0.5) {
+                sad.addFirst(i);
+                solution.addFirst(i);
+                message += "addFirst" + "(" + i + ")" + "\n";
+            } else {
+                sad.addLast(i);
+                solution.addLast(i);
+                message += "addLast" + "(" + i + ")" + "\n";
+            }
+        }
+
+        for (int i = 0; i < 15; i++) {
+            double flag = StdRandom.uniform();
+            Integer a, b;
+            if (flag > 0.5) {
+                a = sad.removeFirst();
+                b = solution.removeFirst();
+                message += "removeFirst" + "(" + i + ")" + "\n";
+            } else {
+                a = sad.removeLast();
+                b = solution.removeLast();
+                message += "removeLast" + "(" + i + ")" + "\n";
+            }
+            System.out.println(a + " " + b);
+            assertEquals(message, a, b);
+        }
+    }
+}
